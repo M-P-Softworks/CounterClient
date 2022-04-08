3b3047bda1501d2302cae6aa547facb85e9ca87d
presnov <presnov@aqsi.ru>
2022-04-08 12:35:34 +0300

    feat: added some UI functionality

--- app/src/main/java/com/emotionfactry/counterclient/presentation/counter/CounterFragment.kt
+++ app/src/main/java/com/emotionfactry/counterclient/presentation/counter/CounterFragment.kt
@@ -5,24 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.emotionfactry.counterclient.databinding.FragmentCounterBinding
 import com.emotionfactry.counterclient.databinding.FragmentSettingsBinding
 
 class CounterFragment : Fragment() {
 
-    private lateinit var binding: FragmentSettingsBinding
+    private lateinit var binding: FragmentCounterBinding
+    private val viewModel: CounterViewModel by viewModels()
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        navController = findNavController()
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentSettingsBinding.bind(view)
+        binding = FragmentCounterBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
     }
+
+
 
     override fun onResume() {
         super.onResume()
@@ -33,6 +44,14 @@ class CounterFragment : Fragment() {
     }
 
     private fun observeViewModel(){
-
+        binding.decreaseBtn.setOnClickListener {
+            viewModel.decrementValue()
+        }
+        binding.increaseBtn.setOnClickListener {
+            viewModel.incrementValue()
+        }
+        viewModel.counterValue.observe(viewLifecycleOwner){
+            binding.counterValue.text = it.toString()
+        }
     }
 }
