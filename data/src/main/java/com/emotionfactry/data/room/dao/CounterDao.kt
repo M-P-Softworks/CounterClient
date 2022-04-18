c68863e3deb392538e37875b9ae507bf9351c8a7
presnov <presnov@aqsi.ru>
2022-04-18 16:48:01 +0300

    feat: DI counter use case DI

--- data/src/main/java/com/emotionfactry/data/room/dao/CounterDao.kt
+++ data/src/main/java/com/emotionfactry/data/room/dao/CounterDao.kt
@@ -6,25 +6,27 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.emotionfactry.data.model.Counter
 import com.emotionfactry.data.model.CounterGroup
+import com.emotionfactry.data.room.entity.CounterEntity
+import com.emotionfactry.data.room.entity.CounterGroupEntity
 import java.util.*
 
 @Dao
 interface CounterDao {
 
     @Query("SELECT * FROM Counter")
-    fun getCounters(): List<Counter>
+    fun getCounters(): List<CounterEntity>
 
     @Query("SELECT * FROM CounterGroups")
-    fun getGroups(): List<CounterGroup>
+    fun getGroups(): List<CounterGroupEntity>
 
     @Query("SELECT * " +
             "FROM Counter " +
             "WHERE Counter.GroupID = :groupID")
-    fun getCurrentCounters(groupID: UUID): List<Counter>
+    fun getCurrentCounters(groupID: UUID): List<CounterEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNewCounter(insert: Counter)
+    fun insertNewCounter(insert: CounterEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNewCounterGroup(insert: CounterGroup)
+    fun insertNewCounterGroup(insert: CounterGroupEntity)
 }
